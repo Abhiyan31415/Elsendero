460ab5cacd181df09b3befc75a887fe27c9540bd
Abhiyan31415 <abhiyankhanal7@gmail.com>
2026-08-13 02:08:53 +0545

    added skip browser warning

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ContextProvider from './context/ContextProvider.jsx'
+import axios from 'axios';
+
+// Add this line so ALL axios requests bypass the ngrok warning screen
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
